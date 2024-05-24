77a030dc0998aba3ff565d5fb1d728c31aaf9429
touhidcodes <touhidcodes@gmail.com>
2024-05-24 15:21:14 +0600

    init: axios jwt-decode, feat: axios instance, token set in local storage

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,9 +1,10 @@
+import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: `${process.env.BACKEND_URL}` }),
+  baseQuery: axiosBaseQuery({ baseUrl: `${process.env.BACKEND_URL}` }),
   endpoints: (builder) => ({}),
 });
 
