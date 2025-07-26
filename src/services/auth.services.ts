750de99d9d45aec564d30601f3dfd4c72df05ebb
touhidcodes <touhidcodes@gmail.com>
2025-07-26 23:30:34 +0600

    fix: local server url

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -40,7 +40,8 @@ export const removeUser = () => {
 
 export const getNewAccessToken = async () => {
   return await axiosInstance({
-    url: "http://localhost:5000/api/v1/refresh-token",
+    url: `${process.env.NEXT_PUBLIC_LOCAL_URL}/refresh-token`,
+    // url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/refresh-token`,
     method: "POST",
     headers: { "Content-Type": "application/json" },
     withCredentials: true,
